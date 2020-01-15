c8cbf4067cad11558ca7781ad27713416672d6fd
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-01-15 11:59:47 -0600

    update landing to check gh-actions validity

--- isaac-lewis-80degrees/src/pages/HomePage/index.jsx
+++ isaac-lewis-80degrees/src/pages/HomePage/index.jsx
@@ -8,12 +8,12 @@ const HomePage = () => {
 
         <div className="flex flex-column justify-center items-center mh4 mt5 image ">
         <a target='_blank' rel='noopener noreferrer' className='link white dim' href="https://ditto.fm/80-degrees-velvet">
-            <figure className='cover-image ma0'>
+            <figure className='cover-image ma0 bg-yellow'>
                 <img src={cover} alt ="80DegreesVelvetCover" />
             </figure>
             <div className='tc'>
                 <h3 className='f3-ns f6'>
-                    Listen to 80 Degrees / Velvet Here
+                    Listen to 80 Degrees / Velvet
                 </h3>
             </div>
         </a>
