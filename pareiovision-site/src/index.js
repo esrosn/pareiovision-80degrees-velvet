31de4c78e89477808dea75bb257150de8b9a2109
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-04-23 18:51:01 -0500

    update

--- pareiovision-site/src/index.js
+++ pareiovision-site/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/index.scss';
+import '../node_modules/tachyons/css/tachyons.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
