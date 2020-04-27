65e27cd354118e18e2cd4787352ff1d970b7294b
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-04-27 10:55:57 -0500

    Update moving to s3 stuff

--- pareiovision-site/src/Components/Nav/index.jsx
+++ pareiovision-site/src/Components/Nav/index.jsx
@@ -7,6 +7,36 @@ export default class Nav extends Component {
     isMenuOpen: false,
   };
   images = [];
+
+  
+
+  handleMouseOver = () => {
+    this.images.forEach((image, idx) => {
+      if (idx === 0) {
+        return;
+      } else if (idx === 1) {
+        image.style.display = `block`;
+        image.style.transform = `translateY(2rem)`;
+      } else {
+        image.style.display = `block`;
+        image.style.transform = `translateY(4rem)`;
+      }
+    });
+  };
+  handleMouseOut = () => {
+    this.images.forEach((image, idx) => {
+      if (idx === 0) {
+        return;
+      } else if (idx === 1) {
+        image.style.display = '';
+        image.style.transform = '';
+      } else {
+        image.style.display = '';
+        image.style.transform = '';
+      }
+    });
+  };
+
   toggleMenu = () => {
     let isMenuOpen = !this.state.isMenuOpen;
     this.setState({ isMenuOpen }, () => {
@@ -28,43 +58,46 @@ export default class Nav extends Component {
     return (
       <nav className='w-100 z-2'>
         <ul className='list pa0 mt1 mb0 mh4 flex flex-row justify-between items-center-l white-80 f3  lh-copy fw7'>
-          <li>
-            <Link to='/home' className='link white-80'>
-              <h1 className='ma0 f2 f3-l'>ZAYLAND</h1>
-            </Link>
+          <li className='w-25'>
             <Link
-              // onMouseOver={this.handleMouseOver}
-              // onMouseOut={this.handleMouseOut}
+              onMouseOver={this.handleMouseOver}
+              onMouseOut={this.handleMouseOut}
               to='/'
-              className='link dim w-50-m logo-image'
+              className='link'
             >
-              <img
-                src={logo}
-                alt=''
-                ref={(ref) => {
-                  this.images[0] = ref;
-                }}
-              />
-              <img
-                src={logo}
-                alt=''
-                ref={(ref) => {
-                  this.images[1] = ref;
-                }}
-              />
-              <img
-                src={logo}
-                alt=''
-                ref={(ref) => {
-                  this.images[2] = ref;
-                }}
-              />
-              {/*</div>*/}
+              <div className='logo-image'>
+                <img
+                  src={logo}
+                  className='w-50'
+                  alt=''
+                  ref={(ref) => {
+                    this.images[0] = ref;
+                  }}
+                />
+                <img
+                  src={logo}
+                  className='w-50'
+                  alt=''
+                  ref={(ref) => {
+                    this.images[1] = ref;
+                  }}
+                />
+                <img
+                  src={logo}
+                  className='w-50'
+                  alt=''
+                  ref={(ref) => {
+                    this.images[2] = ref;
+                  }}
+                />
+              </div>
             </Link>
           </li>
           <li onClick={this.toggleMenu}>
             <div id='menu-icon' className='f2 fw9 pointer menu-button'>
-              <span style={{ marginBottom: '1rem' }}>+</span>
+              <span style={{ marginBottom: '1rem' }}>
+
+              </span>
             </div>
           </li>
         </ul>
