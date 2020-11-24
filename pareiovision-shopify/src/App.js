e1e4338b73fab98eeeada7321bbb6f98be161839
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-11-24 16:03:03 -0600

    Adds slow down and chew content

--- pareiovision-shopify/src/App.js
+++ pareiovision-shopify/src/App.js
@@ -1,140 +1,140 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Products from './components/Products';
 // import Cart from './components/Cart';
-import logo from '../assets/Pareiovision-blk.png';
+import logo from "../assets/Pareiovision-blk.png";
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      isCartOpen: false,
-      checkout: { lineItems: [] },
-      products: [],
-      shop: {},
-    };
+		this.state = {
+			isCartOpen: false,
+			checkout: { lineItems: [] },
+			products: [],
+			shop: {},
+		};
 
-    this.handleCartClose = this.handleCartClose.bind(this);
-    this.addVariantToCart = this.addVariantToCart.bind(this);
-    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
-    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
-  }
+		this.handleCartClose = this.handleCartClose.bind(this);
+		this.addVariantToCart = this.addVariantToCart.bind(this);
+		this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+		this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+	}
 
-  options = {
-    animate: true,
-    patternWidth: 485.5,
-    patternHeight: 485.5,
-    grainOpacity: 0.15,
-    grainDensity: 1,
-    grainWidth: 0.75,
-    grainHeight: 0.75,
-  };
+	options = {
+		animate: true,
+		patternWidth: 485.5,
+		patternHeight: 485.5,
+		grainOpacity: 0.15,
+		grainDensity: 1,
+		grainWidth: 0.75,
+		grainHeight: 0.75,
+	};
 
-  componentWillMount() {
-    this.props.client.checkout.create().then((res) => {
-      this.setState({
-        checkout: res,
-      });
-    });
+	componentWillMount() {
+		this.props.client.checkout.create().then((res) => {
+			this.setState({
+				checkout: res,
+			});
+		});
 
-    this.props.client.product.fetchAll().then((res) => {
-      this.setState({
-        products: res,
-      });
-    });
+		this.props.client.product.fetchAll().then((res) => {
+			this.setState({
+				products: res,
+			});
+		});
 
-    this.props.client.shop.fetchInfo().then((res) => {
-      this.setState({
-        shop: res,
-      });
-    });
-  }
+		this.props.client.shop.fetchInfo().then((res) => {
+			this.setState({
+				shop: res,
+			});
+		});
+	}
 
-  addVariantToCart(variantId, quantity) {
-    this.setState({
-      isCartOpen: true,
-    });
+	addVariantToCart(variantId, quantity) {
+		this.setState({
+			isCartOpen: true,
+		});
 
-    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }];
-    const checkoutId = this.state.checkout.id;
+		const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }];
+		const checkoutId = this.state.checkout.id;
 
-    return this.props.client.checkout
-      .addLineItems(checkoutId, lineItemsToAdd)
-      .then((res) => {
-        this.setState({
-          checkout: res,
-        });
-      });
-  }
+		return this.props.client.checkout
+			.addLineItems(checkoutId, lineItemsToAdd)
+			.then((res) => {
+				this.setState({
+					checkout: res,
+				});
+			});
+	}
 
-  updateQuantityInCart(lineItemId, quantity) {
-    const checkoutId = this.state.checkout.id;
-    const lineItemsToUpdate = [
-      { id: lineItemId, quantity: parseInt(quantity, 10) },
-    ];
+	updateQuantityInCart(lineItemId, quantity) {
+		const checkoutId = this.state.checkout.id;
+		const lineItemsToUpdate = [
+			{ id: lineItemId, quantity: parseInt(quantity, 10) },
+		];
 
-    return this.props.client.checkout
-      .updateLineItems(checkoutId, lineItemsToUpdate)
-      .then((res) => {
-        this.setState({
-          checkout: res,
-        });
-      });
-  }
+		return this.props.client.checkout
+			.updateLineItems(checkoutId, lineItemsToUpdate)
+			.then((res) => {
+				this.setState({
+					checkout: res,
+				});
+			});
+	}
 
-  removeLineItemInCart(lineItemId) {
-    const checkoutId = this.state.checkout.id;
+	removeLineItemInCart(lineItemId) {
+		const checkoutId = this.state.checkout.id;
 
-    return this.props.client.checkout
-      .removeLineItems(checkoutId, [lineItemId])
-      .then((res) => {
-        this.setState({
-          checkout: res,
-        });
-      });
-  }
+		return this.props.client.checkout
+			.removeLineItems(checkoutId, [lineItemId])
+			.then((res) => {
+				this.setState({
+					checkout: res,
+				});
+			});
+	}
 
-  handleCartClose() {
-    this.setState({
-      isCartOpen: false,
-    });
-  }
+	handleCartClose() {
+		this.setState({
+			isCartOpen: false,
+		});
+	}
 
-  render() {
-    return (
-      <div className='vh-100 bg-busy flex flex-column '>
-        <header className='w-100 flex flex-row justify-between items-center ph2 pt2'>
-          <div className='w-25 w-50-m'>
-            <a href='http://www.pareiovision.com' className='link dim  mh4'>
-              <img src={logo} alt='' className='w-100 w-50-l' />
-            </a>
-          </div>
-          {!this.state.isCartOpen && (
-            <div className='flex justify-center items-center mh4-l mt4-l mt2 '>
-              <button
-                disabled
-                className='App__view-cart black f3 fw7 tracked'
-                onClick={() => this.setState({ isCartOpen: true })}
-              >
-                CART
-              </button>
-            </div>
-          )}
-        </header>
-        <div className='vh-75 flex flex-column justify-center items-center'>
-          <h1 className='f3 fw5'>STORE CLOSED</h1>
-          <h2 className='f1 fw7'>
-            <a
-              rel='noopener noreferrer'
-              target='_blank'
-              className='link hover-dark-blue'
-              href='https://ditto.fm/keepingbusy'
-            >
-              LISTEN TO KEEPING BUSY
-            </a>
-          </h2>
-        </div>
-        {/* <Products
+	render() {
+		return (
+			<div className="vh-100 bg-busy flex flex-column ">
+				<header className="w-100 flex flex-row justify-between items-center ph2 pt2">
+					<div className="w-25 w-50-m">
+						<a href="http://www.pareiovision.com" className="link dim  mh4">
+							<img src={logo} alt="" className="w-100 w-50-l" />
+						</a>
+					</div>
+					{!this.state.isCartOpen && (
+						<div className="flex justify-center items-center mh4-l mt4-l mt2 ">
+							<button
+								disabled
+								className="App__view-cart black f3 fw7 tracked"
+								onClick={() => this.setState({ isCartOpen: true })}
+							>
+								CART
+							</button>
+						</div>
+					)}
+				</header>
+				<div className="vh-75 flex flex-column justify-center items-center">
+					<h1 className="f3 fw5">STORE CLOSED</h1>
+					<h2 className="f1 fw7">
+						<a
+							rel="noopener noreferrer"
+							target="_blank"
+							className="link hover-dark-blue"
+							href="https://lnkfi.re/slowdownandchew"
+						>
+							LISTEN TO SLOW DOWN AND CHEW
+						</a>
+					</h2>
+				</div>
+				{/* <Products
           products={this.state.products}
           client={this.props.client}
           addVariantToCart={this.addVariantToCart}
@@ -146,9 +146,9 @@ class App extends Component {
           updateQuantityInCart={this.updateQuantityInCart}
           removeLineItemInCart={this.removeLineItemInCart}
         /> */}
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
